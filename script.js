5121e3107ad01d225e0944f7ebb6bfc32ab5302d
Roesn <Bubwub04@icloud.com>
2023-09-27 15:48:39 -0400

    commenting out the logic of what I want to do in script.js file

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// global arrays
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,8 +9,46 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+// main func
+function generatePassword(){
+
+  // declare empty arrays
+
+  // prompt for length
+
+  // prompt for special char
+
+  // add special char array
+
+  // prompt for numbers
+
+  // add numbers
+
+  // prompt for lower case
+
+  // add lower case array
+
+  // prompt for upper case
+
+  // add upper case array
+
+  // get random charcters in new array
+
+
+  // print securePass array on page
 
 }
 
+// function to add elements to array
+
+
+// function to get random number 
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// call main func
+
